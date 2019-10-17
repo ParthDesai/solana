7a331fb6c3497028da8cc8cb062b07879c6cfb8f
Parth Desai <desaiparth08@gmail.com>
2019-10-17 23:04:52 +0530

    some more style changes

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1609,8 +1609,8 @@ impl Bank {
             let message = &tx.message();
             let acc = raccs.as_ref().unwrap();
 
-            for (i, rent) in acc.3.iter().enumerate() {
-                if message.is_debitable(i) {
+            for (j, rent) in acc.3.iter().enumerate() {
+                if message.is_debitable(j) {
                     collected_rent += *rent;
                 }
             }
