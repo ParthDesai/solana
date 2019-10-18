5b53a7e2d3617c2b7436b86e6b0a10335556459d
Parth Desai <desaiparth08@gmail.com>
2019-10-18 22:18:54 +0530

    finding the root cause

--- runtime/src/accounts.rs
+++ runtime/src/accounts.rs
@@ -619,17 +619,15 @@ impl Accounts {
                 );
             }
             let credit = lock.credits.load(Ordering::Relaxed);
-            if credit > 0 || lock.rent_debtor.load(Ordering::Relaxed) {
-                let mut account = self
-                    .load_slow(ancestors, &pubkey)
-                    .map(|(account, _)| account)
-                    .unwrap_or_default();
-                if lock.rent_debtor.load(Ordering::Relaxed) {
-                    total_rent_collected += rent_collector.update(&mut account);
-                }
-                account.lamports += credit;
-                self.store_slow(fork, &pubkey, &account);
+            let mut account = self
+                .load_slow(ancestors, &pubkey)
+                .map(|(account, _)| account)
+                .unwrap_or_default();
+            if lock.rent_debtor.load(Ordering::Relaxed) {
+                total_rent_collected += rent_collector.update(&mut account);
             }
+            account.lamports += credit;
+            self.store_slow(fork, &pubkey, &account);
         }
         total_rent_collected
     }
