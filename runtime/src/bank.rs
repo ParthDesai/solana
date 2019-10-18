630561939e7edd4bdbc1d98784599279fe45bb7e
Parth Desai <desaiparth08@gmail.com>
2019-10-18 11:09:31 +0530

    unused variable

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1614,7 +1614,7 @@ impl Bank {
             let message = &tx.message();
             let acc = raccs.as_ref().unwrap();
 
-            for (i, rent) in acc
+            for (_i, rent) in acc
                 .3
                 .iter()
                 .enumerate()
